5cacfc7532a4783afa391f5586dd4517e1d089b6
L <d5f1y2m00n@gmail.com>
2021-03-23 14:36:47 +0800

    update tg link

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -120,7 +120,7 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/turkeyfinance21",
+        href: "https://t.me/turkeyfinance",
       },
     ],
   },
